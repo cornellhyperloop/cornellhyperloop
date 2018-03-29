7395f52731c5bc1f84e0752b9f663852fb39e61c
Aneesh1212 <32101485+Aneesh1212@users.noreply.github.com>
2018-03-29 15:45:53 -0400

    PHP

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php $title = 'Home'; ?>
+<?php $metaTags = 'tag1 tag2'; ?>
+<?php $currentPage = 'index'; ?>
+<?php include('head.php'); ?>
+<?php include('nav-bar.php'); ?>
+<body>
+Index Page, Home has an active class
