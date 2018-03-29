04a79d2a26851fb4ce1d3530522e7cfd3f9f4659
Aneesh1212 <32101485+Aneesh1212@users.noreply.github.com>
2018-03-29 00:31:11 -0400

    Add check for login

--- app.js
+++ app.js
@@ -35,18 +35,13 @@
    });
  };
 
+ firebase.auth().onAuthStateChanged(firebaseUser =>{
+  if (firebaseUser){
+    console.log(firebaseUser);
+  } else{
+    console.log("Not logged in");
+  }
+ });
   
-
-  if (btnSignup){
-   btnSignup.addEventListener('click', e=> {
-    const email = txtEmail.value;
-    const pass = txtPassword.value;
-    const auth = firebase.auth();
-
-    const promise = auth.createUserWithEmailAndPassword(email, pass);
-    window.alert("Sorry Wrong Username and Password");
-    promise.catch(e=> console.log(e.message));
-   })
- };
 
 }());
